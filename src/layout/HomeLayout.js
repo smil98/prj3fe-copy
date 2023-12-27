5f693d0c87091bec3b517f698570421e6893d91f
smil98 <seungmilee98@gmail.com>
2023-12-27 13:58:44 +0900

    MemberSignUp : 패스워드 규칙 추가, 에러 메시지 출력 문구 방식 수정, ToolTip 추가, 버튼 크기 수정, 소셜 가입 버튼 추가

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -17,23 +17,6 @@ export function HomeLayout() {
         <Outlet />
       </Box>
       <Spacer h={100} />
-      <Box
-        w="100%"
-        h="150px"
-        mt={5}
-        textAlign="center"
-        backgroundColor="grey"
-        color="white"
-      >
-        <Box mt={15}>
-          <Text mt={10} mb={2}>
-            Company: 뮤레코드 대표: 주예린
-          </Text>
-          <Text mb={2}>E-mail: muerecords@gmail.com</Text>
-          <Text mb={2}>Address: 제주도 서귀포시</Text>
-          <Text mb={2}>Bank info: 국민은행 284002-04-192958</Text>
-        </Box>
-      </Box>
     </>
   );
 }
--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -1,4 +1,5 @@
 import {
+  AbsoluteCenter,
   Box,
   Button,
   ButtonGroup,
@@ -6,52 +7,71 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Center,
+  Divider,
   Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Heading,
   HStack,
+  IconButton,
   Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  MenuItemOption,
   Radio,
   RadioGroup,
+  Select,
   Spacer,
   Stack,
+  Text,
+  Tooltip,
   useToast,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../axiosInstance";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faInfo, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  CheckCircleIcon,
+  EmailIcon,
+  LockIcon,
+  QuestionIcon,
+  SmallCloseIcon,
+  ViewIcon,
+  ViewOffIcon,
+} from "@chakra-ui/icons";
 
 export function MemberSignup() {
   const [logId, setLogId] = useState("");
   const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
   const [gender, setGender] = useState("Unidentified");
   const [birthDate, setBirthDate] = useState("");
-  const [firstDigit, setFirstDigit] = useState("");
   const [role, setRole] = useState("");
-  const [emailAvailable, setEmailAvailable] = useState(false);
-  const [idAvailable, setIdAvailable] = useState(false);
-  const [passwordCheck, setPasswordCheck] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  // 사용 가능 여부 체크
+  const [emailValid, setEmailValid] = useState(false);
+  const [idValid, setIdValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [passwordChecked, setPasswordChecked] = useState(false);
+
+  let sameOriginEmail = false;
+  let emailChecked = sameOriginEmail || emailValid;
+  let sameOriginId = false;
+  let idChecked = sameOriginId || idValid;
+
   const toast = useToast();
   const navigate = useNavigate();
-  let sameOriginEmail = false;
-  let emailChecked = sameOriginEmail || emailAvailable;
-  let sameOriginId = false;
-  let idChecked = sameOriginId || idAvailable;
-
-  let passwordChecked = false;
-  if (passwordCheck === password) {
-    passwordChecked = true;
-  }
-  if (password.length === 0) {
-    passwordChecked = true;
-  }
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
 
   function handleSubmit() {
     axiosInstance
@@ -60,11 +80,8 @@ export function MemberSignup() {
         password,
         email,
         name,
-        address,
         gender,
         birthDate: parseInt(birthDate, 10),
-        firstDigit: parseInt(firstDigit, 10),
-        // gender: role,
       })
       .then(() => {
         toast({
@@ -100,7 +117,7 @@ export function MemberSignup() {
         params: params,
       })
       .then(() => {
-        setEmailAvailable(false);
+        setEmailValid(false);
         toast({
           description: "이미 사용 중인 email입니다.",
           status: "warning",
@@ -108,7 +125,7 @@ export function MemberSignup() {
       })
       .catch((error) => {
         if (error.response.status === 404) {
-          setEmailAvailable(true);
+          setEmailValid(true);
           toast({
             description: "사용 가능한 email입니다.",
             status: "success",
@@ -125,7 +142,7 @@ export function MemberSignup() {
         params: params,
       })
       .then(() => {
-        setIdAvailable(false);
+        setIdValid(false);
         toast({
           description: "이미 사용 중인 Id입니다.",
           status: "warning",
@@ -133,7 +150,7 @@ export function MemberSignup() {
       })
       .catch((error) => {
         if (error.response.status === 404) {
-          setIdAvailable(true);
+          setIdValid(true);
           toast({
             description: "사용 가능한 Id입니다.",
             status: "success",
@@ -146,73 +163,161 @@ export function MemberSignup() {
     setGender(value);
   };
 
+  //패스워드 점검
+  useEffect(() => {
+    if (password.length > 0) {
+      const hasAlphabet = /[a-zA-Z]/.test(password);
+      const hasSpecialChar = /[+\-!@#$%&*]/.test(password);
+      const hasNumber = /\d/.test(password);
+
+      const combinationsCount = [hasAlphabet, hasSpecialChar, hasNumber].filter(
+        Boolean,
+      ).length;
+
+      if (combinationsCount >= 2 && password.length >= 6) {
+        setPasswordValid(true);
+        setPasswordErrorMessage("");
+      } else {
+        setPasswordValid(false);
+        setPasswordErrorMessage(
+          "비밀번호는 6자리 이상이어야 하며, 영문, 숫자, 특수문자 중 두 가지 이상을 포함해야 합니다.",
+        );
+      }
+    } else {
+      setPasswordValid(false);
+      setPasswordErrorMessage("패스워드를 입력해주세요.");
+    }
+
+    if (rePassword === password) {
+      setPasswordChecked(true);
+    } else {
+      setPasswordChecked(false);
+    }
+  }, [password, rePassword]);
+
   return (
     <Box>
       <Spacer h={10} />
-      {/* card안에 있던 내용//backgroundColor={"#fae0ea"} overflow={"hidden"}*/}
       <Card
         width={{ base: "full", md: "50%", lg: "45%" }}
         ml={{ base: "none", md: "25%", lg: "25%" }}
       >
-        {/*backgroundColor="#b4c0ea":헤더에 있던거*/}
         <CardHeader>
           <Heading textAlign="center">회원 가입</Heading>
+          <Text textAlign="center" size="xs" mt={5}>
+            이미 회원이라면?{"  "}
+            <Button colorScheme="purple" variant="link">
+              로그인
+            </Button>
+          </Text>
         </CardHeader>
         <CardBody>
-          <Stack spacing={4}>
-            <FormControl>
-              <FormLabel>아이디</FormLabel>
-              <HStack spacing={2}>
-                <Input
-                  value={logId}
-                  onChange={(e) => {
-                    setLogId(e.target.value);
-                    setIdAvailable(false);
-                  }}
-                />
-                <Button isDisabled={idChecked} onClick={handleIdCheck}>
-                  중복 확인
-                </Button>
-              </HStack>
-            </FormControl>
-            <FormControl>
-              <FormLabel>비밀번호</FormLabel>
-              <Input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <FormErrorMessage>암호를 입력해 주세요.</FormErrorMessage>
-            </FormControl>
-            {password.length > 0 && (
-              <FormControl isInvalid={!passwordChecked}>
-                <FormLabel>비밀번호 재입력</FormLabel>
-                <Input
-                  type="password"
-                  value={passwordCheck}
-                  onChange={(e) => setPasswordCheck(e.target.value)}
-                />
-                <FormErrorMessage>
-                  패스워드가 일치하지 않습니다
-                </FormErrorMessage>
-              </FormControl>
-            )}
+          <Stack spacing={3}>
             <FormControl>
               <FormLabel>이메일</FormLabel>
-              <HStack spacing={2}>
+              <InputGroup>
                 <Input
                   type="email"
                   value={email}
                   onChange={(e) => {
                     setEmail(e.target.value);
-                    setEmailAvailable(false);
+                    setEmailValid(false);
                   }}
                 />
-                <Button isDisabled={emailChecked} onClick={handleEmailCheck}>
-                  중복 확인
-                </Button>
-              </HStack>
+                <InputLeftElement pointerEvents="none">
+                  <EmailIcon color="gray.300" />
+                </InputLeftElement>
+                <InputRightElement w="6rem">
+                  <Button
+                    w="5rem"
+                    h="1.75rem"
+                    size="sm"
+                    isDisabled={emailChecked}
+                    onClick={handleEmailCheck}
+                  >
+                    중복 확인
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              {emailChecked && (
+                <>
+                  {emailValid ? (
+                    <Text color="green.500" fontSize="sm" mt={2}>
+                      <CheckCircleIcon /> 사용 가능한 이메일입니다
+                    </Text>
+                  ) : (
+                    <Text color="red.500" fontSize="sm" mt={2}>
+                      <SmallCloseIcon /> 사용 불가능한 이메일입니다
+                    </Text>
+                  )}
+                </>
+              )}
             </FormControl>
+            <FormControl isInvalid={password.length > 0 && !passwordValid}>
+              <FormLabel>
+                <HStack>
+                  <Text>비밀번호</Text>{" "}
+                  <Tooltip
+                    hasArrow
+                    placement="right"
+                    label="비밀번호는 6자리 이상이어야 하며, 영문, 숫자, 또는 특수문자 중 두 가지 이상을 포함해야 합니다."
+                    fontSize="xs"
+                  >
+                    <QuestionIcon color="purple" size="xs" />
+                  </Tooltip>
+                </HStack>
+              </FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  placeholder="비밀번호를 입력하세요"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    setRePassword("");
+                  }}
+                />
+                <InputLeftElement pointerEvents="none">
+                  <LockIcon color="gray.300" />
+                </InputLeftElement>
+                <InputRightElement width="6rem">
+                  <IconButton
+                    isDisabled={password.length <= 0}
+                    w="5rem"
+                    h="1.75rem"
+                    size="sm"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                    icon={showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>{passwordErrorMessage}</FormErrorMessage>
+            </FormControl>
+            {passwordValid && (
+              <FormControl isInvalid={!passwordChecked}>
+                <FormLabel>비밀번호 확인</FormLabel>
+                <InputGroup>
+                  <Input
+                    type="password"
+                    value={rePassword}
+                    onChange={(e) => setRePassword(e.target.value)}
+                  />
+                  <InputLeftElement pointerEvents="none">
+                    <LockIcon color="gray.300" />
+                  </InputLeftElement>
+                </InputGroup>
+                {passwordChecked && (
+                  <Text color="green.500" fontSize="sm" mt={2}>
+                    <CheckCircleIcon /> 비밀번호가 일치합니다
+                  </Text>
+                )}
+                <FormErrorMessage>
+                  비밀번호가 일치하지 않습니다
+                </FormErrorMessage>
+              </FormControl>
+            )}
             <FormControl>
               <FormLabel>이름</FormLabel>
               <Flex>
@@ -224,19 +329,6 @@ export function MemberSignup() {
                 />
               </Flex>
             </FormControl>
-            {/*<FormControl>*/}
-            {/*  <FormLabel>주소</FormLabel>*/}
-            {/*  <Flex>*/}
-            {/*    <Input*/}
-            {/*      maxWidth={600}*/}
-            {/*      value={address}*/}
-            {/*      onChange={(e) => {*/}
-            {/*        setAddress(e.target.value);*/}
-            {/*      }}*/}
-            {/*    />*/}
-            {/*  </Flex>*/}
-            {/*</FormControl>*/}
-
             <FormControl>
               <FormLabel htmlFor="birth-date">생년월일</FormLabel>
               <Input
@@ -250,16 +342,12 @@ export function MemberSignup() {
             </FormControl>
             <FormControl>
               <FormLabel>성별</FormLabel>
-              <ButtonGroup
-                onChange={handleGenderChange}
-                isAttached
-                w={{ base: "full", md: "80%", lg: "60%" }}
-              >
+              <ButtonGroup onChange={handleGenderChange} isAttached w="full">
                 <Button
                   borderLeftRadius={20}
                   borderRightRadius={0}
                   borderRightWidth={0}
-                  w="25%"
+                  w="33%"
                   colorScheme={gender == "Male" ? "purple" : "gray"}
                   variant="solid"
                   value="Male"
@@ -271,7 +359,7 @@ export function MemberSignup() {
                 <Button
                   borderTopRadius={0}
                   borderBottomRadius={0}
-                  w="25%"
+                  w="33%"
                   colorScheme={gender == "Female" ? "purple" : "gray"}
                   variant="solid"
                   _focus={{ boxShadow: "none", outline: "none" }}
@@ -283,7 +371,7 @@ export function MemberSignup() {
                   borderLeftRadius={0}
                   borderLeftWidth={0}
                   borderRightRadius={20}
-                  w="50%"
+                  w="33%"
                   colorScheme={gender == "Unidentified" ? "purple" : "gray"}
                   variant="solid"
                   value="Unidentified"
@@ -293,20 +381,8 @@ export function MemberSignup() {
                   밝히고 싶지 않음
                 </Button>
               </ButtonGroup>
-              {/*<FormLabel htmlFor="first-digit">*/}
-              {/*  주민등록번호 뒷자리 첫 번째 숫자*/}
-              {/*</FormLabel>*/}
-              {/*<Input*/}
-              {/*  id="first-digit"*/}
-              {/*  type="text"*/}
-              {/*  placeholder="1"*/}
-              {/*  maxLength={1}*/}
-              {/*  maxWidth={50}*/}
-              {/*  value={firstDigit}*/}
-              {/*  onChange={(e) => setFirstDigit(e.target.value)}*/}
-              {/*/>*/}
             </FormControl>
-            {/*<FormControl isRequired>*/}
+            {/*<FormControl>*/}
             {/*  <FormLabel htmlFor="role">역할</FormLabel>*/}
             {/*  <Select*/}
             {/*    id="role"*/}
@@ -319,12 +395,34 @@ export function MemberSignup() {
             {/*  </Select>*/}
             {/*</FormControl>*/}
           </Stack>
+          <Box position="relative" padding="15">
+            <Divider my={6} />
+            <AbsoluteCenter bg="white" px={5}>
+              <Heading size="sm" color="purple">
+                OR
+              </Heading>
+            </AbsoluteCenter>
+          </Box>
+          <Heading textAlign="center" size="sm" mb={6}>
+            소셜 계정으로 가입하기
+          </Heading>
+          <ButtonGroup
+            w="80%"
+            ml="10%"
+            iconSpacing={5}
+            varaint="outline"
+            size="lg"
+            spacing="4rem"
+            justifyContent="center"
+          >
+            <IconButton isRound={true} icon={<QuestionIcon />} />
+            <IconButton isRound={true} icon={<QuestionIcon />} />
+            <IconButton isRound={true} icon={<QuestionIcon />} />
+          </ButtonGroup>
         </CardBody>
-
-        <CardFooter>
-          <Button onClick={handleSubmit}>
-            {/*backgroundColor="#b4c0ea": 버튼 온 클릭뒤에 있던거*/}
-            회원 가입
+        <CardFooter display="flex" justifyContent="center">
+          <Button onClick={handleSubmit} colorScheme="purple" w="full">
+            회원 가입하기
           </Button>
         </CardFooter>
       </Card>
